63acad5032f69126dfc25bf25ff509fcce565973
mause24 <omararenasuni@gmail.com>
2025-10-21 15:31:42 -0500

    fix: remove the envs from the template repository and sorting the folders of the shared widgets

--- lib/routes/general/general_pages.dart
+++ lib/routes/general/general_pages.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:template_flutter/routes/general/general_routes.dart';
 import 'package:template_flutter/screens/home/home_screen.dart';
-import 'package:template_flutter/shared/splash_screen.dart';
+import 'package:template_flutter/shared/splash_screen/splash_screen.dart';
 
 final GoRouter generalRouter = GoRouter(
   initialLocation: "/splash",
